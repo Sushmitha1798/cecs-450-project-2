d21ea732bd9a0928d6e9a87d6c656cfbeec04325
adamhinkle1 <44854527+adamhinkle1@users.noreply.github.com>
2020-11-16 12:29:24 -0800

    added transition, changed opacity/stroke

--- VS-d3/app.js
+++ VS-d3/app.js
@@ -7,17 +7,18 @@ const svg = canvas.append("svg")
 
 const circle = svg.selectAll("circle");
 
-d3.json('bioGraph/p1GraphData.json').then(data=>{
+d3.json('bioTree/p2TreeData.json').then(data=>{
     circle.data(data)
     .enter().append("circle")
-    .attr("r",(d,i) =>d.duration/25)
+    .transition()
+    .delay(function(d,i){return  d.sequence/1;})
+    .attr("r",(d,i) =>d.duration/30)
     .attr("cx",(d,i) =>d.x/1.5)
     .attr("cy",(d,i)=>d.y/1.5)
     .attr("fill","red")
     .attr("stroke","green")
-    .attr('stroke-width', 1)
-    .attr('fill-opacity',.3);
-    
+    .attr('stroke-width', 0.2)
+    .attr('fill-opacity',.1);
 })
 
 
