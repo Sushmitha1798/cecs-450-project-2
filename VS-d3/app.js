09f562e5e2ce1cae493b78a7a4aa5b47e8ce9932
adamhinkle1 <44854527+adamhinkle1@users.noreply.github.com>
2020-11-19 22:28:09 -0800

    added saccades. updated json values to include x1,x2,y1,y2

--- VS-d3/app.js
+++ VS-d3/app.js
@@ -2,23 +2,35 @@ const canvas = d3.select(".canva");
 
  
 const svg = canvas.append("svg")
-            .attr('width', "80%")
-            .attr('height',800);
+            .attr('width', "100%")
+            .attr('height',"1000");
 
+const speed = 2;
 const circle = svg.selectAll("circle");
-
-d3.json('bioTree/p2TreeData.json').then(data=>{
+const line = svg.selectAll("line");
+d3.json('bioGraph/p1GraphData.json').then(data=>{
     circle.data(data)
     .enter().append("circle")
     .transition()
-    .delay(function(d,i){return  d.sequence/1;})
+    .delay(function(d,i){return  d.sequence * speed;})
     .attr("r",(d,i) =>d.duration/30)
-    .attr("cx",(d,i) =>d.x/1.5)
-    .attr("cy",(d,i)=>d.y/1.5)
+    .attr("cx",(d,i) =>d.x1)
+    .attr("cy",(d,i)=>d.y1/1.5)
     .attr("fill","red")
     .attr("stroke","green")
     .attr('stroke-width', 0.2)
     .attr('fill-opacity',.1);
+
+    line.data(data)
+    .enter().append("line")
+    .transition()
+    .delay(function(d,i){return  d.sequence * speed;})
+    .style("stroke", "indigo")
+    .style("stroke-width", 1)
+    .attr("x1", (d,i)=>d.x1)
+    .attr("y1", (d,i)=>d.y1/1.5)
+    .attr("x2", (d,i)=>d.x2)
+    .attr("y2", (d,i)=>d.y2/1.5) 
+    .attr("stroke-opacity",.08);
+    
 })
-
-
